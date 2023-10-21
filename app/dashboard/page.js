06afc1dee23e2c86f84e8f92737dd7d3fa0e758e
Mackensie Jack <kensiejack1@gmail.com>
2023-10-21 16:23:10 -0400

    style(dashboard): light dash styling and user info

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -4,7 +4,17 @@ import { useSession } from "next-auth/react";
 
 const Dashboard = () => {
   const { data: session } = useSession();
-  return <>dashboard</>;
+  return (
+    <>
+      <div className="flex justify-between">
+        <h2>Hello, {session?.user?.name}</h2>
+        <div className="flex bg-storm gap-2 h-10 rounded-lg pr-10 overflow-hidden items-center">
+          <img src={session?.user?.image} className="w-10 h-10 " />
+          {session?.user?.name}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
